a694531abf86bd4792df2dcce149f7cc6058af19
ayyoubez <ayyoubez@gmail.com>
2024-08-01 23:15:43 +0100

    annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Description:takes a float n as argument and returns the string
+    Arguments: n: float
+'''
+
+
+def to_str(n: float) -> str:
+    ''' Return n as a string. '''
+    return str(n)
